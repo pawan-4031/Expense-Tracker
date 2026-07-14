b061cb6eb436c1be2d323eb2507181fae2d108f6
pawan-4031 <pawankumarlimboo@gmail.com>
2026-07-14 12:30:52 +0530

    add docker files and github actions workflow

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount DECIMAL(10, 2) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    description VARCHAR(255),
+    date DATE NOT NULL
+);
